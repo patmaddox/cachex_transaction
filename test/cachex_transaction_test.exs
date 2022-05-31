017870b13e9332f87a1af3ade2c3dae3a400cf4e
Pat Maddox <pat@ratiopbc.com>
2022-05-31 14:22:39 -0700

    add detail of test failure

--- test/cachex_transaction_test.exs
+++ test/cachex_transaction_test.exs
@@ -11,6 +11,10 @@ defmodule CachexTransactionTest do
   end
 
   test "create a thing" do
+    # fails with:
+    #   no match of right hand side value: {:ok, {:error, "could not checkout the connection owned by #PID<0.328.0>. When using the sandbox, connections are shared,
+    #   so this may imply another process is using a connection. Reason: connection not available and request was dropped from queue after 982ms.
+    #   You can configure how long requests wait in the queue using :queue_target and :queue_interval. See DBConnection.start_link/2 for more information"}}
     assert Thing.create_thing(%{name: "new thing"}).name == "new thing"
   end
 end
