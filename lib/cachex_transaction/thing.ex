6511b2d817999e1c006a4f5f3aac8711f9a2558c
Pat Maddox <pat@ratiopbc.com>
2022-05-31 17:39:36 -0700

    failing tests

--- lib/cachex_transaction/thing.ex
+++ lib/cachex_transaction/thing.ex
@@ -1,28 +1,7 @@
 defmodule CachexTransaction.Thing do
   use Ecto.Schema
-  import Ecto.Changeset
-  alias CachexTransaction.Repo
 
   schema "things" do
     field(:name, :string)
-  end
-
-  def create_thing(attrs) do
-    {:ok, {:ok, %__MODULE__{} = thing}} =
-      Repo.transaction(fn ->
-        Task.async(fn ->
-          attrs
-          |> changeset()
-          |> Repo.insert!()
-        end)
-        |> Task.await()
-      end)
-
-    thing
-  end
-
-  defp changeset(struct \\ %__MODULE__{}, attrs) do
-    struct
-    |> cast(attrs, __schema__(:fields) -- [:id])
   end
 end
--- test/cachex_transaction_test.exs
+++ test/cachex_transaction_test.exs
@@ -1,20 +1,65 @@
 defmodule CachexTransactionTest do
   use ExUnit.Case, async: false
-  doctest CachexTransaction
 
   alias CachexTransaction.Repo
   alias CachexTransaction.Thing
 
-  setup do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
-    Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+  setup tags do
+    {:ok, agent} = Agent.start_link(fn -> %{} end)
+
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Repo, shared: not tags[:async])
+    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+
+    %{agent: agent}
   end
 
-  test "create a thing" do
+  test "new process inside transaction" do
     # fails with:
-    #   no match of right hand side value: {:ok, {:error, "could not checkout the connection owned by #PID<0.328.0>. When using the sandbox, connections are shared,
+    #   could not checkout the connection owned by #PID<0.328.0>. When using the sandbox, connections are shared,
     #   so this may imply another process is using a connection. Reason: connection not available and request was dropped from queue after 982ms.
     #   You can configure how long requests wait in the queue using :queue_target and :queue_interval. See DBConnection.start_link/2 for more information"}}
-    assert Thing.create_thing(%{name: "new thing"}).name == "new thing"
+    Repo.transaction(fn ->
+      Task.async(fn ->
+        Repo.one(Thing)
+      end)
+      |> Task.await()
+    end)
+  end
+
+  test "transaction repo" do
+    Repo.transaction(fn repo ->
+      Task.async(fn ->
+        repo.one(Thing)
+      end)
+      |> Task.await()
+    end)
+  end
+
+  test "existing process inside transaction", %{agent: agent} do
+    Repo.transaction(fn ->
+      Agent.update(agent, fn _ ->
+        Repo.one(Thing)
+      end)
+    end)
+  end
+
+  test "allow existing process", %{agent: agent} do
+    Ecto.Adapters.SQL.Sandbox.allow(Repo, self(), agent)
+
+    Repo.transaction(fn ->
+      Agent.update(agent, fn _ ->
+        Repo.one(Thing)
+      end)
+    end)
+  end
+
+  test "allow existing process with transaction repo", %{agent: agent} do
+    Ecto.Adapters.SQL.Sandbox.allow(Repo, self(), agent)
+
+    Repo.transaction(fn repo ->
+      Agent.update(agent, fn _ ->
+        repo.one(Thing)
+      end)
+    end)
   end
 end
